e9d51ddc429e396d32af58bddb693ab08e21a55a
RUANCELI SANTOS <ruan@celi.se>
2020-03-27 21:00:33 -0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=migracao',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=mariadb-s6vhx-mariadb.mariadb-s6vhx.svc.cluster.local;dbname=com.escola10',
+    'username' => 'admin',
+    'password' => '123456',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
